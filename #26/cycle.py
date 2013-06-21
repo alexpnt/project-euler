f7809d5a867f36e63c76c249812f1e4d1d7b5869
AlexPnt <alexandpinto@gmail.com>
2013-06-21 09:26:28 +0100

    problem 26 added

---
+++ #26/cycle.py
@@ -0,0 +1,30 @@
+#http://projecteuler.net/problem=26
+#Reciprocal cycles
+#by Alexandre Pinto
+
+def findLongestCycle():
+
+	best=0
+
+	for i in range(1000,0,-1):
+		candidate=countCycleLen(1,i)
+		if(candidate>best):				#update best
+			best=candidate
+		if(best>i):						#for a given value i, the max possible length is i-1, so we can guarantee that there isnt any better
+			break
+
+	return best
+
+def countCycleLen(d,D):					#returns the length of the recurring cycle, for a given dividend and divisor
+	remainders=[]
+	r=q=1
+	while r!=0 and r not in remainders:	#when we find a repeated remainder, we have a cycle (or not)
+		q=d/D
+		r=d%D
+		remainders+=[r]					#save the remainders
+		d=r*10
+		r=d%D
+	return len(remainders)
+
+if __name__ == '__main__':
+	print findLongestCycle()
