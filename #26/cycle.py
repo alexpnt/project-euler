6e06a3b7a3332a6a2ac762600fd0cfc4c49cde63
AlexPnt <alexandpinto@gmail.com>
2013-06-21 09:45:58 +0100

    updated problem 26

--- #26/cycle.py
+++ #26/cycle.py
@@ -5,15 +5,17 @@
 def findLongestCycle():
 
 	best=0
+	index=0
 
 	for i in range(1000,0,-1):
 		candidate=countCycleLen(1,i)
 		if(candidate>best):				#update best
 			best=candidate
+			index=i
 		if(best>i):						#for a given value i, the max possible length is i-1, so we can guarantee that there isnt any better
 			break
 
-	return best
+	return index
 
 def countCycleLen(d,D):					#returns the length of the recurring cycle, for a given dividend and divisor
 	remainders=[]
